df17e8c9c1efe0171de328346d22bddc4c4d7d05
Chia-Hung Liu <terryliu@croods.(none)>
2014-09-12 02:15:40 -0700

    fix the code, now it can train SVM with different #cells

--- CrossValid_SVM_balance_elim.m
+++ CrossValid_SVM_balance_elim.m
@@ -2,7 +2,14 @@ clc
 clear
 close all
 
+%cell_sel = [1, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 23, 24, 30, 33, 39, 40, 41, 42, 44, 45, 46, 47, 49, 50, 51, 53, 57, 60, 65, 66, 67, 68, 69, 70, 73, 74, 75, 76, 77, 78, 80, 82, 83, 84, 86, 87, 88, 89, 93, 94, 95, 97, 102, 103, 104, 105, 111, 112, 113, 114, 118, 120, 121, 129, 130, 131, 132, 134, 135, 136, 138, 141, 144, 145, 151, 154, 155, 157, 158, 159, 166, 167, 168, 169, 170, 171, 174, 181, 189, 193];
+%cell_sel = [5, 6, 7, 8, 9, 23, 42, 43, 55, 57, 60, 61, 62, 66, 67, 71];
+cell_sel = [1:199];
+
 def_bin_size = 50;
+def_cell_count = length(cell_sel);
+def_max_pc_count = 400;
+def_plot_pc_count = 50;
 def_balance_coef = 1;
 
 load(['../bindatagen/binMatWord_' int2str(def_bin_size) '.mat'])
@@ -14,7 +21,7 @@ trialSet = [];
 wordSet = [];
 
 for iTrial=1:length(binMat)
-    A = binMat{iTrial}.mat';
+    A = binMat{iTrial}.mat(cell_sel,:)';
     B = binMat{iTrial}.type;
     trialSet = [trialSet; A(:)'];
     wordSet = [wordSet; B];
@@ -22,8 +29,9 @@ end
 
 outfile = fopen('datalog.txt','w');
 
+
 stat_total = {};
-for nPC=1:400
+for nPC=1:def_max_pc_count
 for iWord=1:length(wordtoVerify)
     stat_total{iWord}.word = wordtoVerify{iWord};
     
@@ -70,15 +78,16 @@ for iWord=1:length(wordtoVerify)
             distingSet = strcmp(wordtoVerify{iWord}, workSetw);
 
             options.MaxIter = 150000;
-            svmStruct = svmtrain(scoreSel, distingSet, 'options', options); %, 'showplot',true);
+            svmStruct = svmtrain(scoreSel, distingSet, 'options', options, 'AutoScale', 'off'); %, 'showplot',true);
+            %svmStruct = svmtrain(scoreSel, distingSet, 'options', options); %, 'showplot',true);
             resultSet = svmclassify(svmStruct, verifySet*coeff(:, 1:nPC)); %, 'showplot',true);
             ansSet = strcmp(wordtoVerify{iWord}, verifySetw);
-            hitRate = sum(eq(resultSet, ansSet))/length(ansSet);
-            posHitRate = sum(resultSet(find(ansSet)))/length(find(ansSet));
+            Accuracy = sum(eq(resultSet, ansSet))/length(ansSet);
+            HitRate = sum(resultSet(find(ansSet)))/length(find(ansSet));
 
-            if (hitRate > local_best_score | ((hitRate == local_best_score) & (posHitRate > local_best_scorep)))
-                local_best_score = hitRate;
-                local_best_scorep = posHitRate;
+            if (Accuracy > local_best_score | ((Accuracy == local_best_score) & (HitRate > local_best_scorep)))
+                local_best_score = Accuracy;
+                local_best_scorep = HitRate;
                 SVM_candidate{iTestSet}.svm = svmStruct;
                 SVM_candidate{iTestSet}.pc = coeff(:, 1:nPC);
                 SVM_candidate{iTestSet}.varcov = varCov;
@@ -86,20 +95,16 @@ for iWord=1:length(wordtoVerify)
         end
         resultSet = svmclassify(SVM_candidate{iTestSet}.svm, testSet*SVM_candidate{iTestSet}.pc);
         ansSet = strcmp(wordtoVerify{iWord}, testSetw);
-        hitRate = sum(eq(resultSet, ansSet))/length(ansSet);
-        posHitRate = sum(resultSet(find(ansSet)))/length(find(ansSet));
-        negHitRate = (length(find(ansSet==0)) - sum(resultSet(find(ansSet==0))))/length(find(ansSet==0));
+        Accuracy = sum(eq(resultSet, ansSet))/length(ansSet);
+        HitRate = sum(resultSet(find(ansSet)))/length(find(ansSet));
+        FHitRate = (length(find(ansSet==0)) - sum(resultSet(find(ansSet==0))))/length(find(ansSet==0));
+        d_prime = min([norminv(0.99) norminv(HitRate)]) - max([norminv(0.01) norminv(1 - FHitRate)]);
         
-        fprintf(outfile, 'Word: %s, SVM-%2d, #TestVec: %d, #HR: %f, #pHR: %f, #nHR: %f, #PC:%d\n', wordtoVerify{iWord}, iTestSet, length(ansSet), hitRate, posHitRate, negHitRate, nPC);
-        stat_total{iWord}.data(nPC).HR(iTestSet) = hitRate;
-        stat_total{iWord}.data(nPC).pHR(iTestSet) = posHitRate;
-        stat_total{iWord}.data(nPC).nHR(iTestSet) = negHitRate;
-        
-        % dp calculation
-        HR = min([norminv(0.99) norminv(posHitRate)]);
-        FL = max([norminv(0.01) norminv(1 - negHitRate)]);
-        stat_total{iWord}.data(nPC).dp(iTestSet) = HR - FL; 
-        
+        fprintf(outfile, 'Word: %s, SVM-%2d, #TestVec: %d, #HR: %f, #pHR: %f, #nHR: %f, #PC:%d\n', wordtoVerify{iWord}, iTestSet, length(ansSet), Accuracy, HitRate, FHitRate, nPC);
+        stat_total{iWord}.data(nPC).HR(iTestSet) = Accuracy;
+        stat_total{iWord}.data(nPC).pHR(iTestSet) = HitRate;
+        stat_total{iWord}.data(nPC).nHR(iTestSet) = FHitRate;
+        stat_total{iWord}.data(nPC).DP(iTestSet) = d_prime;       
         stat_total{iWord}.data(nPC).varcov(iTestSet) = SVM_candidate{iTestSet}.varcov;
         stat_total{iWord}.data(nPC).pc{iTestSet} = SVM_candidate{iTestSet}.pc;
         stat_total{iWord}.data(nPC).svm{iTestSet} = SVM_candidate{iTestSet}.svm;
@@ -107,10 +112,11 @@ for iWord=1:length(wordtoVerify)
     stat_total{iWord}.avgHR(nPC) = mean(stat_total{iWord}.data(nPC).HR, 2);
     stat_total{iWord}.avgpHR(nPC) = mean(stat_total{iWord}.data(nPC).pHR, 2);
     stat_total{iWord}.avgnHR(nPC) = mean(stat_total{iWord}.data(nPC).nHR, 2);
+    stat_total{iWord}.avgDP(nPC) = mean(stat_total{iWord}.data(nPC).DP, 2);
     stat_total{iWord}.avgvarcov(nPC) = mean(stat_total{iWord}.data(nPC).varcov, 2);
 end
 
-save(['stat_total_balance_'  int2str(def_bin_size)  'ms.mat'], 'stat_total', '-v7.3')
+save(['stat_total_balance_' int2str(def_cell_count) '_' int2str(def_bin_size)  'ms.mat'], 'stat_total', '-v7.3')
 
 end
 
@@ -118,7 +124,7 @@ fclose(outfile);
 
 set(0, 'DefaultFigureVisible', 'on')
 
-%load(['stat_total_balance_'  int2str(def_bin_size)  'ms.mat']);
+%load(['stat_total_balance_' int2str(def_cell_count) '_' int2str(def_bin_size)  'ms.mat']);
 
 for iWord=1:length(wordtoVerify)
     avgHR = [];
@@ -127,7 +133,7 @@ for iWord=1:length(wordtoVerify)
     avgvarcov = [];
     dp = [];
     
-    for nPC=1:28
+    for nPC=1:def_plot_pc_count
 
         avgHR = [avgHR stat_total{iWord}.data(nPC).HR'];
         avgpHR = [avgpHR stat_total{iWord}.data(nPC).pHR'];
@@ -148,31 +154,31 @@ for iWord=1:length(wordtoVerify)
 
     figure;
     boxplot(avgHR);
-    title(['Accuracy of Two-Way SVM (balance) for "' wordtoVerify{iWord} '" (199u 28pc)']);
+    title(['Accuracy of Two-Way SVM (balance) for "' wordtoVerify{iWord} '" (' int2str(def_cell_count) 'u ' int2str(def_plot_pc_count) 'pc)']);
     xlabel('Number of PCs'); ylabel('Accuracy');
     saveas(gcf, ['./PCA_SVM_balance/Accu_balance_' wordtoVerify{iWord} 'box.jpg'], 'jpg');
 
     figure;
     boxplot(avgpHR);
-    title(['Accuracy (true) of Two-Way SVM (balance) for "' wordtoVerify{iWord} '" (199u 28pc)']);
+    title(['Accuracy (true) of Two-Way SVM (balance) for "' wordtoVerify{iWord} '" (' int2str(def_cell_count) 'u ' int2str(def_plot_pc_count) 'pc)']);
     xlabel('Number of PCs'); ylabel('Accuracy: hit/ (hit+miss)');
     saveas(gcf, ['./PCA_SVM_balance/AccuP_balance_' wordtoVerify{iWord} '_box.jpg'], 'jpg');
 
     figure;
     boxplot(avgnHR);
-    title(['Accuracy (false) of Two-Way SVM (balance) for "' wordtoVerify{iWord} '" (199u 28pc)']);
+    title(['Accuracy (false) of Two-Way SVM (balance) for "' wordtoVerify{iWord} '" (' int2str(def_cell_count) 'u ' int2str(def_plot_pc_count) 'pc)']);
     xlabel('Number of PCs'); ylabel('Accuracy: CR/(CR+FA)');
     saveas(gcf, ['./PCA_SVM_balance/AccuN_balance_' wordtoVerify{iWord} '_box.jpg'], 'jpg');
 
     figure;
     boxplot(avgvarcov);
-    title(['Variance Coverage of Two-Way SVM (balance) for "' wordtoVerify{iWord} '" (199u 28pc)']);
+    title(['Variance Coverage of Two-Way SVM (balance) for "' wordtoVerify{iWord} '" (' int2str(def_cell_count) 'u ' int2str(def_plot_pc_count) 'pc)']);
     xlabel('Number of PCs'); ylabel('Variance Coverage');
     saveas(gcf, ['./PCA_SVM_balance/Varcov_balance_' wordtoVerify{iWord} '_box.jpg'], 'jpg');    
 
     figure;
     boxplot(dp);
-    title(['d-prime for word ' wordtoVerify{iWord} ' (199u 28pc) (balance)']);
+    title(['d-prime for word ' wordtoVerify{iWord} ' (' int2str(def_cell_count) 'u ' int2str(def_plot_pc_count) 'pc) (balance)']);
     xlabel('Number of PCs'); ylabel('Variance Coverage');
     saveas(gcf, ['./PCA_SVM_balance/DP_balance_' wordtoVerify{iWord} '_box.jpg'], 'jpg');
 
